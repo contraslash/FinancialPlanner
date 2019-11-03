fdb103cf2f619c1347c27862181c85e0d26e4869
ma0c <ma0@contraslash.com>
2019-11-02 19:44:18 -0500

    added application context

--- app/src/main/java/com/contraslash/android/financial_planner/applications/core/layouts/MainLayout.kt
+++ app/src/main/java/com/contraslash/android/financial_planner/applications/core/layouts/MainLayout.kt
@@ -6,14 +6,13 @@ import androidx.compose.Composable
 
 import androidx.ui.core.Text
 import androidx.ui.core.dp
-import androidx.ui.layout.Column
-import androidx.ui.layout.CrossAxisAlignment
-import androidx.ui.layout.LayoutSize
-import androidx.ui.layout.Spacing
+import androidx.ui.layout.*
 import androidx.ui.material.Button
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.TopAppBar
 import androidx.ui.tooling.preview.Preview
 import com.contraslash.android.financial_planner.R
+import com.contraslash.android.financial_planner.config.MainApplication
 import com.contraslash.android.financial_planner.config.darkThemeColors
 
 
@@ -24,16 +23,28 @@ fun MainFinancialPlannerLayout() {
         colors = darkThemeColors
     )
     {
-        Column (
+        FlexColumn (
             crossAxisSize = LayoutSize.Expand,
-            crossAxisAlignment = CrossAxisAlignment.Center,
-            modifier = Spacing(16.dp)
+            crossAxisAlignment = CrossAxisAlignment.Center
         ) {
-            Button (
-                text = Resources.getSystem().getString(R.string.app_name)
-            )
+            inflexible {
+                TopAppBar(
+                    title = {
+                        Text(text = MainApplication.applicationContext().getString(R.string.app_name))
+                    }
+                )
+            }
+            flexible(flex = 1f) {
 
+                Button (
+                    text = MainApplication.applicationContext().getString(R.string.open_messages)
+                )
+
+
+            }
         }
+
+
     }
 
 }
