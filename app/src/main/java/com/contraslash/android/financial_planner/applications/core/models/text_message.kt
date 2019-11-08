2193c803fbdb74b55735db873d6fa0fca02be6d0
Mauricio Collazos <ma0@contraslash.com>
2019-11-08 14:25:00 -0500

    room uncatible with compose

--- app/src/main/java/com/contraslash/android/financial_planner/applications/core/models/text_message.kt
+++ app/src/main/java/com/contraslash/android/financial_planner/applications/core/models/text_message.kt
@@ -1,25 +1,32 @@
 package com.contraslash.android.financial_planner.applications.core.models
 
 import androidx.compose.Model
-import androidx.room.*
+//import androidx.room.*
+
+//@Entity(tableName = "sms")
+//@Model
+//data class SMS(
+//    @ColumnInfo(name = "body") val body: String,
+//    @ColumnInfo(name = "originatingAddress")val originatingAddress: String,
+//    @PrimaryKey @ColumnInfo(name = "timestamp") val timestamp: Long
+//)
 
 @Model
-@Entity(tableName = "sms")
 data class SMS(
-    @ColumnInfo(name = "body") val body: String,
-    @ColumnInfo(name = "originatingAddress")val originatingAddress: String,
-    @PrimaryKey @ColumnInfo(name = "timestamp") val timestamp: Long
+    val body: String,
+    val originatingAddress: String,
+    val timestamp: Long
 )
 
-@Dao
-interface WordDao {
-
-    @Query("SELECT * from sms ORDER BY timestamp DESC")
-    fun getAllSMS(): List<SMS>
-
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(sms: SMS)
-
-    @Query("DELETE FROM sms")
-    suspend fun deleteAll()
-}
+//@Dao
+//interface WordDao {
+//
+//    @Query("SELECT * from sms ORDER BY timestamp DESC")
+//    fun getAllSMS(): List<SMS>
+//
+//    @Insert(onConflict = OnConflictStrategy.IGNORE)
+//    suspend fun insert(sms: SMS)
+//
+//    @Query("DELETE FROM sms")
+//    suspend fun deleteAll()
+//}
