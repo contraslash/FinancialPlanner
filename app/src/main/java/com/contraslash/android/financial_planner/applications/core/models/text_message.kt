553162ea92e712fbc19d35a37cc1572de2fb4f01
ma0c <ma0@contraslash.com>
2019-11-07 19:38:25 -0500

    first approaches to room

--- app/src/main/java/com/contraslash/android/financial_planner/applications/core/models/text_message.kt
+++ app/src/main/java/com/contraslash/android/financial_planner/applications/core/models/text_message.kt
@@ -1,10 +1,25 @@
 package com.contraslash.android.financial_planner.applications.core.models
 
 import androidx.compose.Model
+import androidx.room.*
 
 @Model
+@Entity(tableName = "sms")
 data class SMS(
-    val body: String,
-    val originatingAddress: String,
-    val timestamp: Long
+    @ColumnInfo(name = "body") val body: String,
+    @ColumnInfo(name = "originatingAddress")val originatingAddress: String,
+    @PrimaryKey @ColumnInfo(name = "timestamp") val timestamp: Long
 )
+
+@Dao
+interface WordDao {
+
+    @Query("SELECT * from sms ORDER BY timestamp DESC")
+    fun getAllSMS(): List<SMS>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(sms: SMS)
+
+    @Query("DELETE FROM sms")
+    suspend fun deleteAll()
+}
